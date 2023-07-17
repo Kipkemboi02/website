2eb001dee66eaf0e942f3ca746ce41343ffdad90
Kipkemboi <emmanuelkipkemboi185@gmail.com>
2023-07-17 11:03:28 +0300

    validation

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ let emailClass = document.getElementsByClassName("email");
 let errorMsg = document.getElementsByClassName("error-msg");
 let mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
 
-function checker()
+function checker(){
 if(emailClass.nodeValue.match(mailRegex)){
     errorMsg.style.display = "none";
-    emailClass.style.border = '2px solid #2ecc71';
+    emailcl.style.border = '2px solid #2ecc71';
 
 }
 else if(emailClass.value == ""){
     errorMsg.style.display = "none";
-    emailClass.style.border = '2px solid #2ecc71';
+    emailClass.style.border='2px solid #2ecc71';
 
 }
 else{
-    errorMsg.style.display = "block";
-    emailClass.style.border = '2px solid #2ecc71';
+    errorMsg.style.display= "block";
+    emailClass.style.border= '2px solid #2ecc71';
+}
 }
